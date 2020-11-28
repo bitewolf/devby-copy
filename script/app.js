7ed2b80d3d1d801400ce3966f96de9b7385b8e4d
bitewolf <2189108@gmail.com>
2020-11-28 10:30:36 +0300

    произошла пагинация
    произошла пагинация

--- script/app.js
+++ script/app.js
@@ -2,9 +2,9 @@
 
 const news__blockis = document.querySelectorAll(".news__block");
 
-let data =["file:///C:/to_sho_dev_by/img/top-new.jpg","Что гуглят во время пандемии. 13 графиков ","Как коронавирус повлиял на поисковые запросы? Как менялся пользовательский интерес, к тем или иным товарам и услугам? 13 графиков отвечают. Речь идёт об англоязычных, запросах в Google с марта по ноябрь 2020 года.","1488 комментариев","Коронавирус","file:///C:/to_sho_dev_by/img/new1.jpg","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","file:///C:/to_sho_dev_by/img/new2.jpg","Youtube представил «бесконечный клип» на популярную песню","","","Коронавирус","file:///C:/to_sho_dev_by/img/new3.jpg","Инди-разработчик выпустил на PC бесплатный симулятор PS5","","1 комментарий","","file:///C:/to_sho_dev_by/img/new4.jpg","Начальника службы безопасности Apple обвинили в попытке дать взятку айпадами за, разрешение на ношение оружия","","1 комментарий","Коронавирус","file:///C:/to_sho_dev_by/img/new5.jpg","Tesla Model X взломали за 90 секунд через уязвимости брелока и Bluetooth","","","Коронавирус","file:///C:/to_sho_dev_by/img/top-new.jpg","Что гуглят во время пандемии. 13 графиков ","Как коронавирус повлиял на поисковые запросы? Как менялся пользовательский интерес, к тем или иным товарам и услугам? 13 графиков отвечают. Речь идёт об англоязычных, запросах в Google с марта по ноябрь 2020 года.","1488 комментариев","Коронавирус","file:///C:/to_sho_dev_by/img/new1.jpg","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","file:///C:/to_sho_dev_by/img/new1.jpg","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","file:///C:/to_sho_dev_by/img/new1.jpg","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","file:///C:/to_sho_dev_by/img/new1.jpg","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","file:///C:/to_sho_dev_by/img/new1.jpg","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус"];
+let data =["img/top-new.jpg","Что гуглят во время пандемии. 13 графиков ","Как коронавирус повлиял на поисковые запросы? Как менялся пользовательский интерес, к тем или иным товарам и услугам? 13 графиков отвечают. Речь идёт об англоязычных, запросах в Google с марта по ноябрь 2020 года.","1488 комментариев","Коронавирус","img/new1.jpg","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","img/new2.jpg","Youtube представил «бесконечный клип» на популярную песню","","","Коронавирус","img/new3.jpg","Инди-разработчик выпустил на PC бесплатный симулятор PS5","","1 комментарий","","img/new4.jpg","Начальника службы безопасности Apple обвинили в попытке дать взятку айпадами за, разрешение на ношение оружия","","1 комментарий","Коронавирус","img/new5.jpg","Tesla Model X взломали за 90 секунд через уязвимости брелока и Bluetooth","","","Коронавирус","img/top-new.jpg","Что гуглят во время пандемии. 13 графиков ","Как коронавирус повлиял на поисковые запросы? Как менялся пользовательский интерес, к тем или иным товарам и услугам? 13 графиков отвечают. Речь идёт об англоязычных, запросах в Google с марта по ноябрь 2020 года.","1488 комментариев","Коронавирус","https://place-hold.it/267x150/fff/c00?text=zdarova&bold=true&fontsize=36","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","https://place-hold.it/267x150/fff/c00?text=pidor&bold=true&fontsize=36","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","https://place-hold.it/267x150/fff/c00?text=ebuchiy&bold=true&fontsize=36","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","https://place-hold.it/267x150/fff/c00?text=nu sho&bold=true&fontsize=36","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус","https://place-hold.it/267x150/fff/c00?text=ty?&bold=true&fontsize=36","2 наших коллег в СИЗО КГБ, 9 — проходят по уголовным делам. Свободу журналистам!","","1 комментарий","Коронавирус"];
 
-    console.log (data);
+   // console.log (data);
 
 //let i = 0;
 //for (let yobaKek of news__blockis) {
@@ -15,11 +15,13 @@ let data =["file:///C:/to_sho_dev_by/img/top-new.jpg","Что гуглят во 
 //    i = i+1;
 //}
 let pages = data.length/30;
-console.log (pages);
+//console.log (pages);
 for (let pageCount = 0; pageCount < pages; pageCount++) {
     //Создание страницы новостей.
     let news__page = document.createElement('div');
     news__page.className = "news__blocks"+pageCount;
+
+
 //Добавление страницы новостей в верстку
     let news__blocksContainer = document.querySelector('.news__blocks-container');
     news__blocksContainer.appendChild(news__page);
@@ -69,21 +71,33 @@ for (let pageCount = 0; pageCount < pages; pageCount++) {
 
 }
 
+let zalupa = document.querySelector('.news__blocks0').classList.add('visible');//отображение первой страницы
 
 
-
+//жалкие попытки присрать пагинацию
 for (let i = 0; i < pages; i++){
 
-
+let news__blocksAll = document.querySelector('.news__blocks-container').querySelectorAll('div');
 let news__blocks = document.querySelector('.news__blocks'+i);
+
+let pageClickAll = document.querySelector('.news__footer-pages').querySelectorAll('li');
 let pageClick = document.querySelector('#page'+i);
 
 pageClick.addEventListener("click", function() {
-    news__blocks.classList.add('visible');
-    pageClick.classList.add('active');
 
+for (let pageDel of pageClickAll){                  //удаление класса active со всех кнопок
+pageDel.classList.remove('active');
+
+};
+for (let blockDel of news__blocksAll){                  //удаление класса active со всех кнопок
+blockDel.classList.remove('visible');
+
+};
+
+    news__blocks.classList.add('visible');
+       pageClick.classList.add('active');           //добавление класса active на кнопку
 });
-console.log (news__blocks, pageClick);
+
 }
 
 
